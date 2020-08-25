8123f29f355dc17ee3d07e21da7b5ae9642e8ca6
AllKrispy <70009510+AllKrispy@users.noreply.github.com>
2020-08-25 15:59:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,410 @@
+// C++ program to Check divisibility by numbers 1-100
+#include<stdio.h>
+int main()
+{
+    int num=0;
+    printf("Enter a no.: ");
+    scanf("%d",&num);
+
+    if(num%1==0)
+        puts("Divisible by 1");
+    else
+        puts("Not Divisible by 1");
+    if(num%2==0)
+        puts("Divisible by 2");
+    else
+        puts("Not Divisible by 2");
+    if(num%3==0)
+        puts("Divisible by 3");
+    else
+        puts("Not Divisible by 3");
+    if(num%4==0)
+        puts("Divisible by 4");
+    else
+        puts("Not Divisible by 4");
+    if(num%5==0)
+        puts("Divisible by 5");
+    else
+        puts("Not Divisible by 5");
+    if(num%6==0)
+        puts("Divisible by 6");
+    else
+        puts("Not Divisible by 6");
+    if(num%7==0)
+        puts("Divisible by 7");
+    else
+        puts("Not Divisible by 7");
+    if(num%8==0)
+        puts("Divisible by 8");
+    else
+        puts("Not Divisible by 8");
+    if(num%9==0)
+        puts("Divisible by 9");
+    else
+        puts("Not Divisible by 9");
+    if(num%10==0)
+        puts("Divisible by 10");
+    else
+        puts("Not Divisible by 10");
+    if(num%11==0)
+        puts("Divisible by 11");
+    else
+        puts("Not Divisible by 11");
+    if(num%12==0)
+        puts("Divisible by 12");
+    else
+        puts("Not Divisible by 12");
+    if(num%13==0)
+        puts("Divisible by 13");
+    else
+        puts("Not Divisible by 13");
+    if(num%14==0)
+        puts("Divisible by 14");
+    else
+        puts("Not Divisible by 14");
+    if(num%15==0)
+        puts("Divisible by 15");
+    else
+        puts("Not Divisible by 15");
+    if(num%16==0)
+        puts("Divisible by 16");
+    else
+        puts("Not Divisible by 16");
+    if(num%17==0)
+        puts("Divisible by 17");
+    else
+        puts("Not Divisible by 17");
+    if(num%18==0)
+        puts("Divisible by 18");
+    else
+        puts("Not Divisible by 18");
+    if(num%19==0)
+        puts("Divisible by 19");
+    else
+        puts("Not Divisible by 19");
+    if(num%20==0)
+        puts("Divisible by 20");
+    else
+        puts("Not Divisible by 20");
+    if(num%21==0)
+        puts("Divisible by 21");
+    else
+        puts("Not Divisible by 21");
+    if(num%22==0)
+        puts("Divisible by 22");
+    else
+        puts("Not Divisible by 22");
+    if(num%23==0)
+        puts("Divisible by 23");
+    else
+        puts("Not Divisible by 23");
+    if(num%24==0)
+        puts("Divisible by 24");
+    else
+        puts("Not Divisible by 24");
+    if(num%25==0)
+        puts("Divisible by 25");
+    else
+        puts("Not Divisible by 25");
+    if(num%26==0)
+        puts("Divisible by 26");
+    else
+        puts("Not Divisible by 26");
+    if(num%27==0)
+        puts("Divisible by 27");
+    else
+        puts("Not Divisible by 27");
+    if(num%28==0)
+        puts("Divisible by 28");
+    else
+        puts("Not Divisible by 28");
+    if(num%29==0)
+        puts("Divisible by 29");
+    else
+        puts("Not Divisible by 29");
+    if(num%30==0)
+        puts("Divisible by 30");
+    else
+        puts("Not Divisible by 30");
+    if(num%31==0)
+        puts("Divisible by 31");
+    else
+        puts("Not Divisible by 31");
+    if(num%32==0)
+        puts("Divisible by 32");
+    else
+        puts("Not Divisible by 32");
+    if(num%33==0)
+        puts("Divisible by 33");
+    else
+        puts("Not Divisible by 33");
+    if(num%34==0)
+        puts("Divisible by 34");
+    else
+        puts("Not Divisible by 34");
+    if(num%35==0)
+        puts("Divisible by 35");
+    else
+        puts("Not Divisible by 35");
+    if(num%36==0)
+        puts("Divisible by 36");
+    else
+        puts("Not Divisible by 36");
+    if(num%37==0)
+        puts("Divisible by 37");
+    else
+        puts("Not Divisible by 37");
+    if(num%38==0)
+        puts("Divisible by 38");
+    else
+        puts("Not Divisible by 38");
+    if(num%39==0)
+        puts("Divisible by 39");
+    else
+        puts("Not Divisible by 39");
+    if(num%40==0)
+        puts("Divisible by 40");
+    else
+        puts("Not Divisible by 40");
+    if(num%41==0)
+        puts("Divisible by 41");
+    else
+        puts("Not Divisible by 41");
+    if(num%42==0)
+        puts("Divisible by 42");
+    else
+        puts("Not Divisible by 42");
+    if(num%43==0)
+        puts("Divisible by 43");
+    else
+        puts("Not Divisible by 43");
+    if(num%44==0)
+        puts("Divisible by 44");
+    else
+        puts("Not Divisible by 44");
+    if(num%45==0)
+        puts("Divisible by 45");
+    else
+        puts("Not Divisible by 45");
+    if(num%46==0)
+        puts("Divisible by 46");
+    else
+        puts("Not Divisible by 46");
+    if(num%47==0)
+        puts("Divisible by 47");
+    else
+        puts("Not Divisible by 47");
+    if(num%48==0)
+        puts("Divisible by 48");
+    else
+        puts("Not Divisible by 48");
+    if(num%49==0)
+        puts("Divisible by 49");
+    else
+        puts("Not Divisible by 49");
+    if(num%50==0)
+        puts("Divisible by 50");
+    else
+        puts("Not Divisible by 50");
+    if(num%51==0)
+        puts("Divisible by 51");
+    else
+        puts("Not Divisible by 51");
+    if(num%52==0)
+        puts("Divisible by 52");
+    else
+        puts("Not Divisible by 52");
+    if(num%53==0)
+        puts("Divisible by 53");
+    else
+        puts("Not Divisible by 53");
+    if(num%54==0)
+        puts("Divisible by 54");
+    else
+        puts("Not Divisible by 54");
+    if(num%55==0)
+        puts("Divisible by 55");
+    else
+        puts("Not Divisible by 55");
+    if(num%56==0)
+        puts("Divisible by 56");
+    else
+        puts("Not Divisible by 56");
+    if(num%57==0)
+        puts("Divisible by 57");
+    else
+        puts("Not Divisible by 57");
+    if(num%58==0)
+        puts("Divisible by 58");
+    else
+        puts("Not Divisible by 58");
+    if(num%59==0)
+        puts("Divisible by 59");
+    else
+        puts("Not Divisible by 59");
+    if(num%60==0)
+        puts("Divisible by 60");
+    else
+        puts("Not Divisible by 60");
+    if(num%61==0)
+        puts("Divisible by 61");
+    else
+        puts("Not Divisible by 61");
+    if(num%62==0)
+        puts("Divisible by 62");
+    else
+        puts("Not Divisible by 62");
+    if(num%63==0)
+        puts("Divisible by 63");
+    else
+        puts("Not Divisible by 63");
+    if(num%64==0)
+        puts("Divisible by 64");
+    else
+        puts("Not Divisible by 64");
+    if(num%65==0)
+        puts("Divisible by 65");
+    else
+        puts("Not Divisible by 65");
+    if(num%66==0)
+        puts("Divisible by 66");
+    else
+        puts("Not Divisible by 66");
+    if(num%67==0)
+        puts("Divisible by 67");
+    else
+        puts("Not Divisible by 67");
+    if(num%68==0)
+        puts("Divisible by 68");
+    else
+        puts("Not Divisible by 68");
+    if(num%69==0)
+        puts("Divisible by 69");
+    else
+        puts("Not Divisible by 69");
+    if(num%70==0)
+        puts("Divisible by 70");
+    else
+        puts("Not Divisible by 70");
+    if(num%71==0)
+        puts("Divisible by 71");
+    else
+        puts("Not Divisible by 71");
+    if(num%72==0)
+        puts("Divisible by 72");
+    else
+        puts("Not Divisible by 72");
+    if(num%73==0)
+        puts("Divisible by 73");
+    else
+        puts("Not Divisible by 73");
+    if(num%74==0)
+        puts("Divisible by 74");
+    else
+        puts("Not Divisible by 74");
+    if(num%75==0)
+        puts("Divisible by 75");
+    else
+        puts("Not Divisible by 75");
+    if(num%76==0)
+        puts("Divisible by 76");
+    else
+        puts("Not Divisible by 76");
+    if(num%77==0)
+        puts("Divisible by 77");
+    else
+        puts("Not Divisible by 77");
+    if(num%78==0)
+        puts("Divisible by 78");
+    else
+        puts("Not Divisible by 78");
+    if(num%79==0)
+        puts("Divisible by 79");
+    else
+        puts("Not Divisible by 79");
+    if(num%80==0)
+        puts("Divisible by 80");
+    else
+        puts("Not Divisible by 80");
+    if(num%81==0)
+        puts("Divisible by 81");
+    else
+        puts("Not Divisible by 81");
+    if(num%82==0)
+        puts("Divisible by 82");
+    else
+        puts("Not Divisible by 82");
+    if(num%83==0)
+        puts("Divisible by 83");
+    else
+        puts("Not Divisible by 83");
+    if(num%84==0)
+        puts("Divisible by 84");
+    else
+        puts("Not Divisible by 84");
+    if(num%85==0)
+        puts("Divisible by 85");
+    else
+        puts("Not Divisible by 85");
+    if(num%86==0)
+        puts("Divisible by 86");
+    else
+        puts("Not Divisible by 86");
+    if(num%87==0)
+        puts("Divisible by 87");
+    else
+        puts("Not Divisible by 87");
+    if(num%88==0)
+        puts("Divisible by 88");
+    else
+        puts("Not Divisible by 88");
+    if(num%89==0)
+        puts("Divisible by 89");
+    else
+        puts("Not Divisible by 89");
+    if(num%90==0)
+        puts("Divisible by 90");
+    else
+        puts("Not Divisible by 90");
+    if(num%91==0)
+        puts("Divisible by 91");
+    else
+        puts("Not Divisible by 91");
+    if(num%92==0)
+        puts("Divisible by 92");
+    else
+        puts("Not Divisible by 92");
+    if(num%93==0)
+        puts("Divisible by 93");
+    else
+        puts("Not Divisible by 93");
+    if(num%94==0)
+        puts("Divisible by 94");
+    else
+        puts("Not Divisible by 94");
+    if(num%95==0)
+        puts("Divisible by 95");
+    else
+        puts("Not Divisible by 95");
+    if(num%96==0)
+        puts("Divisible by 96");
+    else
+        puts("Not Divisible by 96");
+    if(num%97==0)
+        puts("Divisible by 97");
+    else
+        puts("Not Divisible by 97");
+    if(num%98==0)
+        puts("Divisible by 98");
+    else
+        puts("Not Divisible by 98");
+    if(num%99==0)
+        puts("Divisible by 99");
+    else
+        puts("Not Divisible by 99");
+    if(num%100==0)
+        puts("Divisible by 100");
+    else
+        puts("Not Divisible by 100");
+    return 0;
+}
